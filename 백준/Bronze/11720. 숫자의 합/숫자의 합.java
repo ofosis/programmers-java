18be4570402579867f76dec85eaa27f9f3a92e33
ofosis <133728145+ofosis@users.noreply.github.com>
2024-02-11 21:36:37 +0900

    [Bronze IV] Title: 숫자의 합, Time: 208 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int count=sc.nextInt();
+		String num=sc.next();
+		int sum=0;
+		for(int i=0; i<count; i++) {
+			sum+=num.charAt(i)-'0';
+		}
+		System.out.println(sum);
+	}
+}
